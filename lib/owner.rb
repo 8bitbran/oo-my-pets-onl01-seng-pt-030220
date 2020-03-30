31849dc89f4e74fa4164b681d6484d96b7f74ca9
Brandon Marrero <brandonmar97@gmail.com>
2020-03-30 22:46:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,7 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{dogs.count} dog(s), and #{cats.count} cats(s)."
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
   end
   
   def save 
