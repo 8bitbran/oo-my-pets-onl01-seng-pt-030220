64b8271f485416b67260b07379bee224fea4bf51
Brandon Marrero <brandonmar97@gmail.com>
2020-03-30 22:12:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,53 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+    @@all.count
+  end 
+  
+  def self.reset_all
+    @@all.clear()
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @species = 'human'
+    @cats = []
+    @dogs = []
+    save
+  end 
+  
+  def say_species 
+   "I am a #{self.species}."
+  end
+  
+  def cats 
+    @cats
+  end 
+  
+  def dogs 
+    @dogs
+  end 
+  
+  def buy_cat(name)
+    @cats << Cat.new(name, self)
+  end 
+  
+  def buy_dog(name)
+    @dogs << Dog.new(name, self)
+  end
+  
+  
+  
+  def save 
+    @@all << self
+  end
 end
